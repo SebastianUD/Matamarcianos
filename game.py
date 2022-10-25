98c081fd217eb1dff827d8e1cf530d2df9ac93bf
SebastianUD <spardot@correo.udistrital.edu.co>
2022-10-24 23:09:50 -0500

    v0.03

--- game.py
+++ game.py
@@ -6,27 +6,27 @@ import pygame
 class Game:
     
     def __init__(self):
-        self.ship=Ship()
-        self.width=800
-        self.height=800
-        self.mySky=Sky(self.width, self.height, 1600)
-        self.screen=pygame.display.set_mode((self.width,self.height))
-        self.clock=pygame.time.Clock()
-        self.fps=60
-        #Cargar la hoja de imágenes
-        self.sprites= pygame.image.load("Matamarcianos/sprites.png")
-        self.shipsprite=pygame.Surface((64,64)).convert()
-        self.shipsprite.blit(self.sprites,(0,0),(250,436,64,64))
+        self.ship=Ship() #Crear la nave
+        self.width=800 #Ancho de la pantalla
+        self.height=800 #Alto de la pantalla
+        self.mySky=Sky(self.width, self.height, 1600) #Crear el cielo
+        self.screen=pygame.display.set_mode((self.width,self.height)) #Crear la pantalla
+        self.clock=pygame.time.Clock() #Crear el reloj para controlar los fps
+        self.fps=60 
+        self.sprites= pygame.image.load("Matamarcianos/sprites.png") #Cargar la hoja de imágenes
+        self.shipsprite=pygame.Surface((64,64)).convert() #Crear una superficie para la nave
+        self.shipsprite.blit(self.sprites,(0,0),(250,436,64,64)) #Cortar la nave de la hoja de imágenes
 
         
     def checkKeys(self):
         keys=pygame.key.get_pressed()
-        if keys[pygame.K_RIGHT]: self.ship.direction="RIGHT"
+        if keys[pygame.K_RIGHT]: self.ship.direction="RIGHT" 
         elif keys[pygame.K_LEFT]: self.ship.direction="LEFT"
         else: self.ship.direction="STOP"
-        #definir los limites de la pantalla
-        if self.ship.x > self.width-64: self.ship.x=self.width-64
-        if self.ship.x < 8: self.ship.x=8
+        
+        #Definir los limites de la pantalla
+        if self.ship.x > self.width-64: self.ship.x=self.width-64 #Limite derecho
+        if self.ship.x < 8: self.ship.x=8 #Limite izquierdo
         
 
     def run (self):
@@ -46,15 +46,15 @@ class Game:
                 b=random.randint(0,255)
                 pygame.draw.circle(self.screen, (r,g,b), star, 1)
             
-            self.mySky.move()
-            self.ship.move()
-            x=self.ship.x
-            y=self.ship.y
-            self.screen.blit(self.shipsprite, (x,y))
-            self.clock.tick(self.fps)
-            self.checkKeys()
-            pygame.display.flip()
+            self.mySky.move() #Mover las estrellas
+            self.ship.move() #Mover la nave
+            x=self.ship.x #Posicion x de la nave
+            y=self.ship.y #Posicion y de la nave
+            self.screen.blit(self.shipsprite, (x,y)) #Dibujar la nave
+            self.clock.tick(self.fps) #Controlar los fps
+            self.checkKeys() #Comprobar las teclas
+            pygame.display.flip() #Actualizar la pantalla
 
-#Siempre hacer un objeto de la clase para que esta haga algo
-myGame=Game()
-myGame.run()     
+
+myGame=Game() #Creamos un objeto de la clase Game
+myGame.run() #Ejecutamos el método run del objeto myGame
--- ship.py
+++ ship.py
@@ -1,13 +1,2 @@
-class Ship:
-    def __init__(self):
-        self.direction = "STOP"
-        self.x=368
-        self.y=710
-    
-    def move(self):
-        if self.direction == "STOP":
-            pass
-        elif self.direction == "LEFT":
-            self.x -= 5
-        elif self.direction == "RIGHT":
-            self.x += 5
+
+            
